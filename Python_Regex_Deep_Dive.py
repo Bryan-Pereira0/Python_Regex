b3cb7dd0b1181ae4237728edcba5cf02391bee94
bryanpereira <gfxmagicz@gmail.com>
2024-10-24 14:45:31 -0400

    Regex

--- Python_Regex_Deep_Dive.py
+++ Python_Regex_Deep_Dive.py
@@ -1,4 +1,6 @@
+#task1
 import re
-text = "HELLO world"
-result = re.search(r'hello', text, re.I)
-print(result)  # Output: <re.Match object; span=(0, 5), match='HELLO'>
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@(?!exclude\.com\b)[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
